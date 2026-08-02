fce96c998a56f7c4ac421d29ae4dd855e07dbe54
Spacastre <spacastreteam@gmail.com>
2026-08-02 09:11:50 +0700

    commit_fixingVercel2

--- app.js
+++ app.js
@@ -41,7 +41,7 @@ app.use("/admin", require("./routes/admin"));
 if (process.env.NODE_ENV !== "production") {
   const PORT = process.env.PORT || 3000;
   app.listen(PORT, () => {
-    console.log(`Server running on http://localhost:3000`);
+    console.log(`Server running on http://localhost:${PORT}`);
   });
 }
 
